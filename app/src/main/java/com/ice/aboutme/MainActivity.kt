a903f0a20b2566ae0f282fc39fa33f832e8c5515
vaibhavjhingran <vaibhavjhingran@gmail.com>
2020-08-21 21:24:04 +0200

    Added data binding capabilities to the app

--- app/src/main/java/com/ice/aboutme/MainActivity.kt
+++ app/src/main/java/com/ice/aboutme/MainActivity.kt
@@ -5,29 +5,33 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.ice.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val myName: MyName = MyName("ice")
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        findViewById<Button>(R.id.submit_btn).setOnClickListener {
+        binding.myName = myName
+
+        binding.submitBtn.setOnClickListener{
             addNickname(it)
         }
     }
 
     private fun addNickname(view: View) {
-        val editText = findViewById<EditText>(R.id.nickname_edit)
-        val nicknameText = findViewById<TextView>(R.id.nickname_text)
-
-        nicknameText.text = editText.text
-        editText.visibility = View.GONE
-        view.visibility = View.GONE
-        nicknameText.visibility = View.VISIBLE
-
+        binding.apply {
+            myName?.nickname = nicknameEdit.text.toString()
+            invalidateAll()
+            nicknameEdit.visibility = View.GONE
+            submitBtn.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
         // Hide the keyboard
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(view.windowToken, 0)
---
+++ app/src/main/java/com/ice/aboutme/MyName.kt
@@ -0,0 +1,6 @@
+package com.ice.aboutme
+
+data class MyName (
+    var name: String = "",
+    var nickname: String = ""
+)
